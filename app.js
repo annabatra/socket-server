7ff508f97904f8eed887e0235dce22ed547062b6
Christoffer <christoffer.bohlin@hotmail.com>
2020-10-08 12:32:03 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// app.js
+const express = require('express');
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.origins(['https://socket-client.jsramverk.me:443']);
+
+io.on('connection', function (socket) {
+    console.info("User connected");
+
+    socket.on('chat message', function (message) {
+        io.emit('chat message', message);
+    });
+});
+
+server.listen(8300);
